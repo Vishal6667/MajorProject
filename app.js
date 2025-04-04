b0d20378ec12d94a1d95a7327c0c8022e2e86e06
Vishal singh <vs478337@gmail.com>
2025-04-04 22:50:22 +0530

    validation

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const path =require('path');
 app.use(express.urlencoded({extended:true}))
 const methodOverride=require('method-override');
 const ejsMate=require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync.js');
+const ExpressError=require('./utils/ExpressError.js')
 const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
+const {listingSchema}=require('./schema.js');
+
 // const imageUrl = decodeURIComponent(req.body.image);
 
 
@@ -45,51 +49,81 @@ app.use(methodOverride('_method'));
 app.engine('ejs',ejsMate);
 
 
-app.get('/listings',async(req,res)=>{
+const validationListing=(req,res,next)=>{
+  let {error}=listingSchema.validate(req.body);
+  
+if(error){
+  let errMsg=error.details.map((el)=>el.message).join(",");
+  throw new ExpressError(404,errMsg)
+}else{
+  next();
+}
+}
+
+//IndexRoute
+
+app.get('/listings',wrapAsync(async(req,res)=>{
 const allListings=await Listing.find({});
 res.render('./listings/index.ejs',{allListings});
-})
+}));
 //New Route
+
 app.get('/listings/new',(req,res)=>{
   res.render('./listings/new.ejs')
-}),
+});
 
 //show route
-app.get('/listings/:id',async(req,res)=>{
+
+app.get('/listings/:id',wrapAsync(async(req,res)=>{
   let {id} =req.params;
   const listing=await Listing.findById(id)
   res.render('./listings/show.ejs',{listing});
-});
+}));
 
 //Create Route
-app.post('/listings',async(req,res)=>{
-// let listing=req.body.listing;
-const newlistings=new Listing(req.body.listing);
-await newlistings.save();
-res.redirect('/listings');
-})
+
+app.post('/listings',validationListing,wrapAsync(async(req,res,next)=>{
+
+    const newlistings=new Listing(req.body.listing);
+    await newlistings.save();
+    res.redirect('/listings');
+}));
 
 //editRoute
-app.get('/listings/:id/edit',async(req,res)=>{
+
+app.get('/listings/:id/edit',wrapAsync(async(req,res)=>{
   let {id} =req.params;
   const listing=await Listing.findById(id)
   res.render('./listings/edit.ejs',{listing});
-})
+}));
 
 //UpdateROute
-app.put('/listings/:id',async (req,res) => {
+
+app.put('/listings/:id',validationListing,wrapAsync(async (req,res) => {
   let {id}=req.params;
   await Listing.findByIdAndUpdate(id,{...req.body.listing})
   res.redirect(`/listings/${id}`);
-})
+}));
+
 //DELETE POST
-app.delete('/listings/:id',async(req,res)=>{
+
+app.delete('/listings/:id',wrapAsync(async(req,res)=>{
   let {id}=req.params;
  const newdelete= await Listing.findByIdAndDelete(id);
  console.log(newdelete);
   res.redirect('/listings')
+}));
+
+app.all("*",(req,res,next)=>{
+  next(new ExpressError(404,"Page not found!"));
 })
 
+
+app.use((err,req,res,next)=>{
+  let{statusCode=500,message="Something went wrong"}=err;
+  res.status(statusCode).render("error.ejs",{err})
+// res.status(statusCode).send(message);
+})
 
 app.get('/',(req,res)=>{
   res.send('working');
--- models/listing.js
+++ models/listing.js
@@ -6,14 +6,16 @@ title:
   required:true
 },
   description:String,
-  image: 
-     String
-    // default:
-    //   "https://unsplash.com/photos/sunflower-field-during-day-time-lk3F07BN8T8",
-    // set: (v) =>
-    //   v === ""
-    //     ? "https://amazingarchitecture.com/storage/files/1/architecture-firms/amin-moazzen/black-fly/black_house_amin_moazzen_bangal_india-3.jpg"
-    //     : v,
+  image: {
+    type: String,
+    default:
+      "https://amazingarchitecture.com/storage/files/1/architecture-firms/amin-moazzen/black-fly/black_house_amin_moazzen_bangal_india-3.jpg",
+    set: (v) =>
+      v === ""
+        ? "https://amazingarchitecture.com/storage/files/1/architecture-firms/amin-moazzen/black-fly/black_house_amin_moazzen_bangal_india-3.jpg"
+        : v,
+  }
+   
   ,
   price:Number,
   location:String,
